e819814ef8d9a04e2e936b479a4227a95f01175d
Artem Klochkov <Arthorror@gmail.com>
2018-12-17 04:14:28 +0300

    add styles

--- html/js/main.js
+++ html/js/main.js
@@ -1,14 +1,15 @@
 $( document ).ready(function() {
     $('.selectpicker').selectpicker();
 
-    var settingsBtn = $('#settingsBtn');
-    var settingsPanel = $('#settingsPanel');
+    var settingsBtn = $('#filtersBtn');
+    var settingsPanel = $('#filtersPanel');
     var closePanel = $('#panelClose');
-    var openPanel = "settings-panel--open";
+    var openPanel = "filters-panel--open";
     console.log('dsadas')
 
     settingsBtn.on('click', function() {
         console.log('clicked')
+        console.log(settingsPanel)
         settingsPanel.hasClass(openPanel) ? settingsPanel.removeClass(openPanel) : settingsPanel.addClass(openPanel);
     });
     closePanel.on('click', function() {
